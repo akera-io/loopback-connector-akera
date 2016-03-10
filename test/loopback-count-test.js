5d762f2ca828b38bf97d3939f609c78b670597f3
Marian Edu <marian.edu@acorn.ro>
2016-03-10 09:39:37 +0200

    added destroyById, exists and update count and rest to new signature
    added support for model mapping to table with different name (alias)
    added support for model properties to table fields mapping (field alias)

--- test/loopback-count-test.js
+++ test/loopback-count-test.js
@@ -2,83 +2,81 @@ var should = require('./ini.js');
 
 var Customer, ds;
 describe('Akera connector', function() {
-	before(function() {
-		ds = getDataSource();
-		Customer = ds.define('Customer', {
-			CustNum: {
-				type: String,
-				id: true,
-				required: true
-			},
-			Name: {
-				type: String
-			},
-			City: {
-				type: String
-			}
-		});
-	});
-	after(function(callback) {
-		ds.disconnect(function(){
-			callback();
-		});
-	});
-	describe('Datasource count method', function() {
-		it('should count the records based on a empty filter', function(done) {
-			Customer.count({}, function(err, nr) {
-				if (err) {
-					done(err);
-					return;
-				}
-				should(nr).be.type('number');
-				done();
-			});
-		});
-		it('should count the records based on a null filter', function(done) {
-			Customer.count(null, function(err, nr) {
-				if (err) {
-					done(err);
-					return;
-				}
-				should(nr).be.type('number');
-				done();
-			});
-		});
-		it('should count the records based on a simple where filter', function(done) {
-			Customer.count({
-				where: {
-					CustNum: {
-						lte: 40
-					}
-				}
-			}, function(err, nr) {
-				if (err) {
-					done(err);
-					return;
-				}
-				should(nr).be.type('number');
-				done();
-			});
-		});
-		it('should count the records based on a complex where filter', function(done) {
-			Customer.count({
-				where: {
-					and: [{
-						CustNum: {
-							lte: 5
-						}
-					}, {
-						Country: "USA"
-					}]
-				}
-			}, function(err, nr) {
-				if (err) {
-					done(err);
-					return;
-				}
-				should(nr).be.type('number');
-				done();
-			});
-		});
-	});
+  before(function() {
+    ds = getDataSource();
+    Customer = ds.define('Customer', {
+      CustNum : {
+        type : String,
+        id : true,
+        required : true
+      },
+      Name : {
+        type : String
+      },
+      City : {
+        type : String
+      }
+    });
+  });
+  after(function(callback) {
+    ds.disconnect(function() {
+      callback();
+    });
+  });
+  describe('Datasource count method', function() {
+    it('should count the records based on a empty filter', function(done) {
+      Customer.count({}, function(err, nr) {
+        if (err) {
+          done(err);
+          return;
+        }
+        should(nr).be.type('number');
+        done();
+      });
+    });
+    it('should count the records based on a null filter', function(done) {
+      Customer.count(null, function(err, nr) {
+        if (err) {
+          done(err);
+          return;
+        }
+        should(nr).be.type('number');
+        done();
+      });
+    });
+    it('should count the records based on a simple where filter',
+        function(done) {
+          Customer.count({
+            CustNum : {
+              lte : 40
+            }
+          }, function(err, nr) {
+            if (err) {
+              done(err);
+              return;
+            }
+            should(nr).be.type('number');
+            done();
+          });
+        });
+    it('should count the records based on a complex where filter', function(
+        done) {
+      Customer.count({
+        and : [ {
+          CustNum : {
+            lte : 5
+          }
+        }, {
+          Country : "USA"
+        } ]
+      }, function(err, nr) {
+        if (err) {
+          done(err);
+          return;
+        }
+        should(nr).be.type('number');
+        done();
+      });
+    });
+  });
 });
--- test/loopback-create-test.js
+++ test/loopback-create-test.js
@@ -2,102 +2,135 @@ var should = require('./ini.js');
 
 var Customer, ds;
 describe('Akera connector', function() {
-	before(function() {
-		ds = getDataSource();
-		Customer = ds.define('Customer', {
-			idInjection:false,
-			CustNum: {
-				type: Number,
-				id: true,
-				required: true
-			},
-			Name: {
-				type: String
-			},
-			City: {
-				type: String
-			}
-		});
-	});
-	after(function(callback) {
-		ds.disconnect(function(){
-			callback();
-		});
-	});
-	describe('Datasource CRUD methods', function() {
-		it('should be able to insert a row in the database', function(done) {
-			var toInsert = {
-				CustNum: 134,
-				Name: 'NodeApi',
-				City: 'Cluj'
-			};
-			Customer.create(toInsert, function(err, iRow) {
-				if (err) {
-					done(err);
-					return;
-				}
-				(iRow.CustNum).should.be.exactly(toInsert.CustNum);
-				(iRow.Name).should.be.exactly(toInsert.Name);
-				(iRow.City).should.be.exactly(toInsert.City);
-				done();
-			});
-		});
+  before(function() {
+    ds = getDataSource();
+    Customer = ds.define('Customer', {
+      idInjection : false,
+      CustNum : {
+        type : Number,
+        id : true,
+        required : true
+      },
+      Name : {
+        type : String
+      },
+      City : {
+        type : String
+      }
+    });
+  });
+  after(function(callback) {
+    ds.disconnect(function() {
+      callback();
+    });
+  });
 
-		it('should throw error when trying to insert if record exists (code 711)', function(done) {
-			this.timeout(3000);
-			Customer.create({
-				CustNum: 134,
-				Name: "NodeApi",
-				City: "Cluj"
-			}, function(err, rsp) {
-				if (err) {
-					(err.code).should.be.exactly(711);
-					done();
-				}else if(rsp){
-					done('Error: records were inserted');
-				}
-			});
-		});
-		var uData;
-		it('should be able to update a row', function(done){
-			uData = {CustNum:134,Name:'UpdateTest',City:"Cluj"};
-			Customer.updateAll({where:{CustNum:134}},uData, function(err, recordArr){
-				if(err){
-					done(err);
-					return;
-				}
-				should(recordArr).be.an.instanceOf(Array);
-				var r =  recordArr[0];
-				(r.CustNum).should.be.exactly(uData.CustNum);
-				(r.Name).should.be.exactly(uData.Name);
-				(r.City).should.be.exactly(uData.City);
-				done();
-			});
-		});
-		it('should be able to update or insert a row (upsert)', function(done){
-			this.timeout(3000);
-			uData = {CustNum:134, Name:'UpsertTest',City:"ClujU"};
-			Customer.upsert(uData, function(err, record){
-				if(err){
-					done(err);
-					return;
-				}
-				should(record).be.an.instanceOf(Object);
-				(record.CustNum).should.be.exactly(uData.CustNum);
-				(record.Name).should.be.exactly(uData.Name);
-				(record.City).should.be.exactly(uData.City);
-				done();
-			});
-		});
-		it('Delete should be able to delete a record ', function(done) {
-			Customer.destroyAll({
-				where: {
-					CustNum: 134
-				}
-			}, function(err, delRows) {
-				(delRows).should.be.exactly(1);
-				done();
-			});
-		});
-	});
+  describe('Datasource CRUD methods', function() {
+    it('should be able to insert a row in the database', function(done) {
+      var toInsert = {
+        CustNum : 134,
+        Name : 'NodeApi',
+        City : 'Cluj'
+      };
+      Customer.create(toInsert, function(err, iRow) {
+        if (err)
+          done(err);
+        else {
+          try {
+            (iRow.CustNum).should.be.exactly(toInsert.CustNum);
+            (iRow.Name).should.be.exactly(toInsert.Name);
+            (iRow.City).should.be.exactly(toInsert.City);
+            done();
+          } catch (err) {
+            done(err);
+          }
+        }
+      });
+    });
+
+    it('should throw error when trying to insert if record exists (code 711)',
+        function(done) {
+          this.timeout(3000);
+          Customer.create({
+            CustNum : 134,
+            Name : "NodeApi",
+            City : "Cluj"
+          }, function(err, rsp) {
+            if (err) {
+              (err.code).should.be.exactly(711);
+              done();
+            } else if (rsp) {
+              done('Error: records were inserted');
+            }
+          });
+        });
+    var uData;
+    it('should be able to update a row', function(done) {
+      uData = {
+        CustNum : 134,
+        Name : 'UpdateTest',
+        City : "Cluj"
+      };
+      Customer.updateAll({
+        where : {
+          CustNum : 134
+        }
+      }, uData, function(err, recordArr) {
+        if (err)
+          done(err);
+        else {
+          try {
+            should(recordArr).be.an.instanceOf(Array);
+            var r = recordArr[0];
+            (r.CustNum).should.be.exactly(uData.CustNum);
+            (r.Name).should.be.exactly(uData.Name);
+            (r.City).should.be.exactly(uData.City);
+            done();
+          } catch (err) {
+            done(err);
+          }
+        }
+      });
+    });
+
+    it('should be able to update or insert a row (upsert)', function(done) {
+      this.timeout(3000);
+      uData = {
+        CustNum : 134,
+        Name : 'UpsertTest',
+        City : "ClujU"
+      };
+      Customer.upsert(uData, function(err, record) {
+        if (err)
+          done(err);
+        else {
+          try {
+            should(record).be.an.instanceOf(Object);
+
+            (record.CustNum).should.be.exactly(uData.CustNum);
+            done();
+          } catch (err) {
+            done(err);
+          }
+        }
+      });
+    });
+
+    it('Delete should be able to delete a record ', function(done) {
+      Customer.destroyAll({
+        CustNum : 134
+      }, function(err, delRows) {
+        if (err)
+          done(err);
+        else {
+          try {
+            (delRows.count).should.be.exactly(1);
+            done();
+          } catch (err) {
+            data(err);
+          }
+        }
+      });
+    });
+  });
 });
